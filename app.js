28fb013917e5f948ca764fbe470235b7d20e47c4
Aldo Valverde <alvane.01@gmail.com>
2018-04-22 20:56:56 -0500

    Adds initial setup for NodeJS project
    Includes dev dependencies, project structure and logs in the app.js file

---
+++ app.js
@@ -0,0 +1,7 @@
+import json from './config/json'
+import { Product, User } from './models';
+
+console.log(json.name);
+
+const product = new Product();
+const user = new User();
---
+++ config/json.js
@@ -0,0 +1,3 @@
+export default {
+  name: "Node.js Homework Application"
+};
